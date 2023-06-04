503392b53909551f00be156a202b5ab93eedf805
f-gorski <55857154+f-gorski@users.noreply.github.com>
2023-06-04 22:28:46 +0200

    feat: fetching task list from notion page

--- netlify/functions/notion.js
+++ netlify/functions/notion.js
@@ -9,17 +9,16 @@ const handler = async () => {
     database_id: process.env.VITE_DB_ID,
   })
 
-  const data = JSON.stringify({
-    name: page.results[0].properties["Task name"].title[0].text.content,
-    start_date: page.results[0].properties.Due.date.start,
+  const tasksList = page.results.map((task) => {
+    return {
+      name: task.properties["Task name"].title[0].text.content,
+      start_date: task.properties.Due.date.start,
+    }
   })
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      name: page.results[0].properties["Task name"].title[0].text.content,
-      start_date: page.results[0].properties.Due.date.start,
-    }),
+    body: JSON.stringify(tasksList),
   }
 }
 
